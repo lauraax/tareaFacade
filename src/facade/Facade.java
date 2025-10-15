e5c264a11c80b705c6fbea0a50eed8d2e97e755c
lauraax <64974256+lauraax@users.noreply.github.com>
2025-10-15 15:00:35 -0500

    Update Facade.java

--- src/facade/Facade.java
+++ src/facade/Facade.java
@@ -5,13 +5,13 @@
  */
 package facade;
 
-import subsystem.FileReader;
-import subsystem.FileWriter;
+import subsystem.LectorArchivo;
+import subsystem.EscritorArchivo;
 import subsystem.FileEncryptor;
 
 public class Facade {
-    private FileReader fileReader;
-    private FileWriter fileWriter;
+    private LectorArchivo fileReader;
+    private EscritorArchivo fileWriter;
     private FileEncryptor fileEncryptor;
 
     // Instancia estática de la fachada, inicializada de manera perezosa (Lazy Initialization)
@@ -19,8 +19,8 @@ public class Facade {
 
     // Constructor privado para evitar instanciación externa
     private Facade() {
-        fileReader = new FileReader();
-        fileWriter = new FileWriter();
+        fileReader = new LectorArchivo();
+        fileWriter = new EscritorArchivo();
         fileEncryptor = new FileEncryptor();
     }
 
@@ -32,12 +32,12 @@ public class Facade {
         return instance;
     }
 
-    public void writeEncryptedFile(String fileName, String content) {
+    public void escribir(String fileName, String content) {
         String encryptedContent = fileEncryptor.encrypt(content);
         fileWriter.write(fileName, encryptedContent);
     }
 
-    public String readDecryptedFile(String fileName) {
+    public String leer(String fileName) {
         String encryptedContent = fileReader.read(fileName);
         return fileEncryptor.decrypt(encryptedContent);
     }
